3729785228004c22d284b9c41697616a47200af3
davichiqueti <davichiqueti@gmail.com>
2023-07-23 22:56:52 -0300

    corrected files

--- EXERCICIOS CAP 4.sql
+++ EXERCICIOS CAP 4.sql
@@ -1,86 +1,85 @@
-/* EXERCICIOS CAPITULO 4 */
+/*	EXERCICIOS CAPITULO 4
 
-/* CRIANDO BANCO E TABELA */
+ 	CRIANDO BANCO E TABELA */
 
 CREATE DATABASE LIBRARY;
 
-CREATE TABLE BOOKS (
-
-	BookName VARCHAR(30),
-	WriterName VARCHAR(30),
-	WriterSex CHAR(1),
-	PagesNum VARCHAR(3),
-	Publisher VARCHAR(30),
-	Price FLOAT(6, 2),
-	State CHAR(2),
-	Year INTEGER(4)
-	
+CREATE TABLE BOOKS(
+	book_name VARCHAR(30),
+	writer_name VARCHAR(30),
+	writer_sex CHAR(1),
+	pages_num VARCHAR(3),
+	publisher VARCHAR(30),
+	price FLOAT(6, 2),
+	state CHAR(2),
+	publish_year INTEGER(4)
 );
-
-/* CORRECOES DO PROFESSOR
-Dar uma margem maior de tamanho para os varchars e para a parte inteira do float de preço
-Numero de paginas declarado como um inteiro
-*/
-
 /* INSERT DOS VALORES  */
 
-INSERT INTO BOOKS(BookName, WriterName, WriterSex, PagesNum, Publisher, Price, State, Year)
+INSERT INTO BOOKS(book_name, writer_name, writer_sex, pages_num, publisher, price, state, year)
 	VALUES('Cavaleiro Real', 'Ana Claudia', 'F', 465, 'Atlas', 49.90, 'RJ', 2009);
 
-INSERT INTO BOOKS(BookName, WriterName, WriterSex, PagesNum, Publisher, Price, State, Year)
+INSERT INTO BOOKS(book_name, writer_name, writer_sex, pages_num, publisher, price, state, year)
 	VALUES('SQL para leigos', 'João Nunes', 'M', 450, 'Addison', 98.00, 'SP', 2018);
 	
-INSERT INTO BOOKS(BookName, WriterName, WriterSex, PagesNum, Publisher, Price, State, Year)
+INSERT INTO BOOKS(book_name, writer_name, writer_sex, pages_num, publisher, price, state, year)
 	VALUES('Receitas Caseiras', 'Celia Tavares', 'F', 210, 'Atlas', 45.00, 'RJ', 2008);
 	
-INSERT INTO BOOKS(BookName, WriterName, WriterSex, PagesNum, Publisher, Price, State, Year)
+INSERT INTO BOOKS(book_name, writer_name, writer_sex, pages_num, publisher, price, state, year)
 	VALUES('Pessoas Efetivas', 'Eduardo Santos', 'M', 310, 'Beta', 78.99, 'RJ', 2018);
 	
-INSERT INTO BOOKS(BookName, WriterName, WriterSex, PagesNum, Publisher, Price, State, Year)
+INSERT INTO BOOKS(book_name, writer_name, writer_sex, pages_num, publisher, price, state, year)
 	VALUES('Habitos Saudáveis', 'Eduardo Santos', 'M', 630, 'Beta', 150.98, 'RJ', 2019);
 	
-INSERT INTO BOOKS(BookName, WriterName, WriterSex, PagesNum, Publisher, Price, State, Year)
+INSERT INTO BOOKS(book_name, writer_name, writer_sex, pages_num, publisher, price, state, year)
 	VALUES('A Casa Marrom', 'Hermes Macedo', 'M', 250, 'Bubba', 60.00, 'MG', 2016);
 	
-INSERT INTO BOOKS(BookName, WriterName, WriterSex, PagesNum, Publisher, Price, State, Year)
+INSERT INTO BOOKS(book_name, writer_name, writer_sex, pages_num, publisher, price, state, year)
 	VALUES('Estacio Querido', 'Geraldo Francisco', 'M', 310, 'Insignia', 100.00, 'ES', 2015);
 	
-INSERT INTO BOOKS(BookName, WriterName, WriterSex, PagesNum, Publisher, Price, State, Year)
+INSERT INTO BOOKS(book_name, writer_name, writer_sex, pages_num, publisher, price, state, year)
 	VALUES('Para sempre amigas', 'Leda Silva', 'F', 510, 'Insignia', 78.98, 'ES', 2011);
 	
-INSERT INTO BOOKS(BookName, WriterName, WriterSex, PagesNum, Publisher, Price, State, Year)
+INSERT INTO BOOKS(book_name, writer_name, writer_sex, pages_num, publisher, price, state, year)
 	VALUES('Copas Inesqueciveis', 'Marco Alcantara', 'M', 200, 'Larson', 130.98, 'RS', 2018);
 	
-INSERT INTO BOOKS(BookName, WriterName, WriterSex, PagesNum, Publisher, Price, State, Year)
+INSERT INTO BOOKS(book_name, writer_name, writer_sex, pages_num, publisher, price, state, year)
 	VALUES('O poder da mente', 'Clara Mafra', 'F', 120, 'Continental', 56.58, 'RS', 2017);
 	
-	
-/* COM O BANCO DE DADOS E A TABELA PRONTA COMEÇA OS EXERCICIOS */
-
-/* 1 - TRAZER TODOS OS DADOS */
+-- EXERCICIO 1 - TRAZER TODOS OS DADOS
 
 SELECT * FROM BOOKS;
 
-/* 2 – Trazer o nome do livro e o nome da editora */
 
-SELECT BookName AS Nome_do_Livro, Publisher AS Nome_da_Editora  FROM BOOKS;
+-- EXERCICIO 2 – Trazer o nome do livro e o nome da editora
+
+SELECT book_name AS Nome_do_Livro, publisher AS Nome_da_Editora
+FROM BOOKS;
 	
-/* 3 – Trazer o nome do livro e a UF dos livros publicados por autores do sexo masculino */
 
-SELECT BookName AS Nome_do_Livro, State AS UF FROM BOOKS
-	WHERE WriterSex = 'M';
+-- EXERCICIO 3 – Trazer o nome do livro e a UF dos livros publicados por autores do sexo masculino
 
-/* 4 - Trazer o nome do livro e o número de páginas dos livros publicados por autores do sexo feminino */
+SELECT book_name AS Nome_do_Livro, state AS UF 
+FROM BOOKS
+WHERE writer_sex = 'M';
 
-SELECT BookName AS Nome_do_Livro, PagesNum AS Paginas FROM BOOKS
-	WHERE WriterSex = 'F';
+
+-- EXERCICIO 4 - Trazer o nome do livro e o número de páginas dos livros publicados por autores do sexo feminino 
+
+SELECT book_name AS Nome_do_Livro, pages_num AS Paginas
+FROM BOOKS
+WHERE writer_sex = 'F';
 	
-/* 5 – Trazer os valores dos livros das editoras de São Paulo */
 
-SELECT BookName AS Nome_do_Livro, Price AS Preço FROM BOOKS
-	WHERE State = 'SP';
+-- EXERCICIO 5 – Trazer os valores dos livros das editoras de São Paulo
 
-/* 6 – Trazer os dados dos autores do sexo masculino que tiveram livros publicados por São Paulo ou Rio de Janeiro (Questão Desafio) */
+SELECT book_name AS Nome_do_Livro, price AS Preço
+FROM BOOKS
+WHERE state = 'SP';
 
-SELECT WriterName AS Nome_do_Autor, WriterSex AS Genero_do_Autor, State AS UF FROM BOOKS
-	WHERE WriterSex = 'M' and (State = 'SP' OR STATE = 'RJ');
+
+-- 6 – Trazer os dados dos autores do sexo masculino que tiveram livros publicados por São Paulo ou Rio de Janeiro (Questão Desafio) */
+
+SELECT writer_name AS Nome_do_Autor, writer_sex AS Genero_do_Autor, state AS UF
+FROM BOOKS
+WHERE writer_sex = 'M' and (state = 'SP' OR state = 'RJ');
--- EXERCICIOS CAP 6.sql
+++ EXERCICIOS CAP 6.sql
@@ -1,52 +1,48 @@
-/* Exercicios Cap 6 */
+/* Exercicios Cap 6 - Fazer consultas pensando em 
 
-/* CRIACAO DE DATABASE E TABELA */
-create database exercicio;
+CRIACAO DE DATABASE E TABELA */
 
-use exercicio;
+create database EXERCISE;
 
-create table funcionarios
+use EXERCISE;
+
+create table employees
   (
-      idFuncionario integer,
-      nome varchar(100),
+      id_employee integer,
+      name varchar(100),
       email varchar(200),
-      sexo varchar(10),
-      departamento varchar(100),
-      admissao varchar(10),
-      salario integer,
-      cargo varchar(100),
-      idRegiao int
+      sex varchar(10),
+      department varchar(100),
+      admission_date varchar(10),
+      salary integer,
+      role varchar(100),
+      id_region int
   );
 
-/* Mostrando apenas as 5 primeiras rows para demonstração */
-
-insert into funcionarios values (1,'Kelley','rkelley0@soundcloud.com','Feminino','Computadores','10/2/2009',67470,'Structural Engineer',2);
-insert into funcionarios values (2,'Armstrong','sarmstrong1@infoseek.co.jp','Masculino','Esporte','3/31/2008',71869,'Financial Advisor',2);
-insert into funcionarios values (3,'Carr','fcarr2@woothemes.com','Masculino','Automotivo','7/12/2009',101768,'Recruiting Manager',3);
-insert into funcionarios values (4,'Murray','jmurray3@gov.uk','Feminino','Joalheria','12/25/2014',96897,'Desktop Support Technician',3);
-insert into funcionarios values (5,'Ellis','jellis4@sciencedirect.com','Feminino','Alimentícios','9/19/2002',63702,'Software Engineer III',7);
-
-
+-- Demonstração 5 primeiras rows. Tabela original com 1000 itens
+insert into employees values (1,'Kelley','rkelley0@soundcloud.com','Feminino','Computadores','10/2/2009',67470,'Structural Engineer',2);
+insert into employees values (2,'Armstrong','sarmstrong1@infoseek.co.jp','Masculino','Esporte','3/31/2008',71869,'Financial Advisor',2);
+insert into employees values (3,'Carr','fcarr2@woothemes.com','Masculino','Automotivo','7/12/2009',101768,'Recruiting Manager',3);
+insert into employees values (4,'Murray','jmurray3@gov.uk','Feminino','Joalheria','12/25/2014',96897,'Desktop Support Technician',3);
+insert into employees values (5,'Ellis','jellis4@sciencedirect.com','Feminino','Alimentícios','9/19/2002',63702,'Software Engineer III',7);
 
 
 /* 
 EXERCICIO 1 
-Traga os funcionarios que trabalhem
+Traga os employees que trabalhem
 no departamento de filmes OU no
 departamento de roupas 
 */
 
-SELECT departamento, COUNT(*)
-FROM FUNCIONARIOS
-GROUP BY departamento;
+SELECT department, COUNT(*)
+FROM employees
+GROUP BY department;
 
--- Resultado do COUNT - departamento de filmes tem 21 registros e o departamento de roupas tem 53 
--- Começaremos o operador OR pelo maior para melhor performance
+-- TEMOS MAIS employees EM ROUPAS, SERÁ O PRIMEIRO VALOR NO OPERADOR OR
 
-SELECT idFuncionario, nome
-FROM FUNCIONARIOS
-WHERE departamento = 'roupas' OR departamento = 'filmes';
-
+SELECT id_employee, name, sex, department
+FROM employees
+WHERE department = 'Roupas' OR department = 'Filmes';
 
 
 /*
@@ -56,45 +52,26 @@ de filmes ou no departamento lar. Ele necessita enviar um email para as colabora
 desses dois setores. 
 */
 
-SELECT departamento, COUNT(*)
-FROM FUNCIONARIOS
-WHERE sexo = 'feminino'
-GROUP BY departamento;
+SELECT department, COUNT(*)
+FROM employees
+GROUP BY department;
 
--- Resultado do COUNT - departamento de filmes tem 9 mulheres e o departamento lar tem 32 mulheres
--- Começaremos com a verificação do departamento pois possui menos valores, retornando FALSO mais rapido
--- Operador OR usara primeiro o departamento com mais mulheres para melhor performance
+-- O SEXO SERÁ O SEGUNDO OPERADOR DO AND, FILTRAR PRIMEIRAMENTE PELO SETOR
+-- TEMOS MAIS employees EM LAR, SERÁ O PRIMEIRO VALOR NO OPERADOR OR
 
-
-SELECT idFuncionario, nome, email
-FROM FUNCIONARIOS
-WHERE (departamento = 'lar' OR departamento = 'filmes') AND sexo = 'feminino';
-
+SELECT id_employee, name, sex, department, email
+FROM employees
+WHERE department = 'Lar' OR department = 'Filmes'
+AND sex = 'Feminino';
 
 
 /*
 EXERCICIO 3
-Traga os funcionarios do sexo masculino
-ou os funcionarios que trabalhem no setor Jardim
+Traga os employees do sexo masculino
+ou os employees que trabalhem no setor Jardim
 */
 
-SELECT sexo, COUNT(*)
-FROM FUNCIONARIOS
-WHERE sexo = 'masculino';
-
-SELECT COUNT(*)
-FROM FUNCIONARIOS
-WHERE departamento = 'Jardim';
-
--- Como o esperado, existem muito mais funcionarios homens (484) do que funcionarios que trabalham no departamento jardim (47)
--- O operador OR terá como primeiro condição o sexo ser masculino, para melhor performance, já que há mais registros
-
-SELECT idFuncionario, nome, departamento
-FROM FUNCIONARIOS
-WHERE sexo = 'masculino' OR departamento = 'Jardim';
-
--- NOTA: Retorno de 507 valores, diferença de 24 da soma total das duas condições,
--- Gerada por rows que condiziam ambos os valores
--- Portanto, há 24 homens no setor Jardim
-
-
+-- O SEXO SERÁ O PRIMEIRO OPERADOR DO OR, POR VALIDAR MAIS FACILMENTE
+SELECT id_employee, name, sex, department
+FROM employees
+WHERE sex = 'Masculino' OR department = 'Jardim';
